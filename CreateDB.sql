12613bedac5f6aee69f4650c79f904eb861cddef
Piotr Witkowski <witus83@gmail.com>
2016-04-11 10:48:55 +0200

    Update CreateDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,11 +1,33 @@
 CREATE TABLE SHOOTERS (
   
-  ID 
+  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   FirstName varchar(50),
   LastName varchar(150),
   City varchar(100),
   Club varchar(100),
   IsLicence boolean,
   DateBirth Date,
+  Email varchar(50),
   RegDate Timestamp
+  
 )
+
+
+CREATE TABLE COMPETITION_TYPE (
+
+  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  DESCR varchar(200)
+  
+)
+
+CREATE TABLE COMPETITIONS (
+
+  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  COMPETITION_NAME varchar(300),
+  MONTH INT(2),
+  YEAR INT(4)
+  
+)
+
+
+
