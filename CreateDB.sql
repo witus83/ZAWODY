f92423be39ebf387a285ff9a1394da4d8e09bdaf
Piotr Witkowski <witus83@gmail.com>
2016-04-11 10:44:28 +0200

    CreateDB.sql

---
+++ CreateDB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SHOOTERS (
+  
+  ID 
+  FirstName varchar(50),
+  LastName varchar(150),
+  City varchar(100),
+  Club varchar(100),
+  IsLicence boolean,
+  DateBirth Date,
+  RegDate Timestamp
+)
