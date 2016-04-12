5098363c715d302cdee0ae2952b0e171e7efbe46
Piotr Witkowski <witus83@gmail.com>
2016-04-12 16:52:19 +0200

    Create Create_procedure.sql

---
+++ Create_procedure.sql
@@ -0,0 +1,24 @@
+CREATE PROCEDURE `insert_shooter`(IN `p_FirstName` VARCHAR(50), IN `p_LastName` VARCHAR(150), IN `p_City` VARCHAR(100), IN `p_Club` VARCHAR(100), IN `p_IsLicence` BOOLEAN, IN `p_BirthDate` DATE, IN `p_Email` VARCHAR(150), IN `p_Phone` VARCHAR(20))
+INSERT INTO SHOOTER (
+	FirstName, 
+	LastName,
+	City,
+    Club,
+    IsLicence,
+    BirthDate,
+    Email,
+    Phone
+) 	
+	values	
+	(
+	p_FirstName,
+	p_LastName,
+	p_City,
+	p_Club,
+	p_IsLicence,
+	p_BirthDate,
+	p_Email,
+	p_Phone
+	)
+	
+	
