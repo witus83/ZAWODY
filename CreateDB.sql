d1a1ac86972197111bbdc76fee4bbe0d9cfd8447
Piotr Witkowski <witus83@gmail.com>
2016-04-15 17:45:38 +0200

    Update CreateDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -3,10 +3,10 @@ CREATE TABLE SHOOTER (
   ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   FirstName varchar(50) not null,
   LastName varchar(150) not null,
-  City varchar(100) not null,
-  Club varchar(100),
+  City varchar(200) not null,
+  Club varchar(1200),
   IsLicence boolean,
-  DateBirth Date,
+  BirthDate Date,
   Email varchar(150),
   Phone varchar(20),
   Notes varchar(1024),
@@ -16,14 +16,18 @@ CREATE TABLE SHOOTER (
 CREATE TABLE USER (
   ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   Login varchar(20),
-  Password varchar(200),
-  Status int(1)
+  Password varchar(300),
+  FirstName varchar(50),
+  LastName varchar(150),
+  Email (varchar(150),
+  Status int(2)
   )
 
 CREATE TABLE COMPETITION_TYPE (
 
   ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  DESCR varchar(200)
+  DESCR varchar(300),
+  Status int(2)
   
 )
 
@@ -31,15 +35,25 @@ CREATE TABLE COMPETITION (
 
   ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   COMPETITION_NAME varchar(300),
-  MONTH INT(2),
-  YEAR INT(4)
+  Date date
 )
 
 CREATE TABLE RESULTS (
-
-  ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  
-  FOREIGN KEY (COMPETITION) REFERENCES COMPETITION(ID),
+  ID INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  P_0 int(2),
+  P_1 int(2),
+  P_2 int(2),
+  P_3 int(2),
+  P_4 int(2),
+  P_5 int(2),
+  P_6 int(2),
+  P_7 int(2),
+  P_8 int(2),
+  P_9 int(2),
+  P_10 int(2),
+  Series int(1),
+  FOREIGN KEY (COMPETITION_TYPE_ID) REFERENCES COMPETITION_TYPE(ID),
+  FOREIGN KEY (COMPETITION_ID) REFERENCES COMPETITION(ID),
   FOREIGN KEY (SHOOTER_ID) REFERENCES SHOOTER(ID)
 )
 
